5c2bbed669fb211a3e17edcb880521adce229fc3
Pavel_Vorobev <pavelvorobev.83@yandex.ru>
2023-08-30 16:08:36 +0300

    task

--- hw_task_1/6.py
+++ hw_task_1/6.py
@@ -1,12 +1,12 @@
 list = [1,2,3,4,5]
 count = 0
-length = len(list)
-def function(length):
-    for number in range(1,6):
-        if number % 2 = 0 in length:
+def function(list):
+    for number in list:
+        if number > 0:
             count +=1
-        else: number += 1
-return(count)
+    return(count)
+
+print(count)
 
 
 
