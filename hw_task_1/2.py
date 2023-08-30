ed95408f54b248333eeb32fda454d070d2e9fcbe
Pavel_Vorobev <pavelvorobev.83@yandex.ru>
2023-08-30 13:47:00 +0300

    task

--- hw_task_1/2.py
+++ hw_task_1/2.py
@@ -1,9 +1,9 @@
-s = str(input())
-s2 = str(input())
+s = list(input())
+s2 = list(input())
 length1 = len(s)
 length2 = len(s2)
 if length1 > length2:
-    print(length1)
+    print(len(s))
 else:
-    print(length2)
+    print(len(s2))
 
