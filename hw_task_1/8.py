60b7609ac5911f939cc2e635b36911a52567b47d
Pavel_Vorobev <pavelvorobev.83@yandex.ru>
2023-08-30 13:32:04 +0300

    task

--- hw_task_1/8.py
+++ hw_task_1/8.py
@@ -1,11 +1,11 @@
 def make_initials(list):
     char_length = len(list)
-    initial = list[0:1]
+    initial = list[0:1] + '.'
     for i in range(char_length):
-        if list[i] == '':
+        if list[i] == ' ':
             initial_location = list[i + 1]
-            initial += '.' + initial_location
-            return initial.upper()
+            initial +=  initial_location + '.'
+    return initial.upper()
 
 list = input("Введите фразу: ")
 
