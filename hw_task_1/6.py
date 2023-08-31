3c80164384578ab42f15faae70b775130f410cde
Pavel_Vorobev <pavelvorobev.83@yandex.ru>
2023-08-31 11:04:12 +0300

    task

--- hw_task_1/6.py
+++ hw_task_1/6.py
@@ -1,12 +1,13 @@
 list = [1,2,3,4,5]
-count = 0
+
 def function(list):
+    count = 0
     for number in list:
         if number > 0:
             count +=1
     return(count)
 
-print(count)
+print(function(list))
 
 
 
