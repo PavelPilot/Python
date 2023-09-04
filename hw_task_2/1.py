045501df10eed6a79dc0d2643f694499c75fd3ff
Pavel_Vorobev <pavelvorobev.83@yandex.ru>
2023-09-04 12:54:08 +0300

    task

--- hw_task_2/1.py
+++ hw_task_2/1.py
@@ -7,6 +7,7 @@ str_1 = 'Python php go C Hello world'
 stop = ['Python', 'php', 'go', 'C']
 str_list = str_1.split(' ')
 
+
 lst = list(filter(lambda x: x not in stop, str_list))
 print(lst)
 print(' '.join(lst))
